b0f336e960b4abd3ef92de43fd56665f2b8f1475
pankajladhar <ladharpankaj@gmail.com>
2018-07-10 14:50:49 +0530

    Added Puzzle component

--- src/Components/Puzzle/index.js
+++ src/Components/Puzzle/index.js
@@ -1,18 +1,36 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import Answer from './../Answer';
 
 class Puzzle extends Component {
-    render() {
-        return (
-            <div>
-                Puzzle
-            </div>
-        );
-    }
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const { data, handleClick, speechSynthesisSupported } = this.props
+    return (
+      <div className="PuzzleContainer">
+        <div className="Puzzle">
+          <h1 className="Puzzle__Statement">
+            What is {data.questionData[0]} + {data.questionData[1]} ?
+          </h1>
+          <div className="Puzzle__Answers">
+            {data.answers.map((answer, i) => {
+              return (
+                <Answer
+                  currentIndex={i+1}
+                  key={`${answer}-${i}`}
+                  value={answer}
+                  speechSynthesisSupported={speechSynthesisSupported}
+                  handleClick={handleClick}
+                />
+              )
+            })}
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
-
-Puzzle.propTypes = {
-
-};
 
 export default Puzzle;

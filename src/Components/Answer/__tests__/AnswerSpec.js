ddc323fdb501083fa5b5d26c48011c368241ab47
pankajladhar <ladharpankaj@gmail.com>
2018-07-16 01:14:49 +0530

    Added test and proptype validation for Answer component

---
+++ src/Components/Answer/__tests__/AnswerSpec.js
@@ -0,0 +1,30 @@
+import React from 'React';
+import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
+import Answer from './../index';
+
+describe('Answer', () => {
+    let answerProps;
+    beforeEach(() => {
+        answerProps = {
+            value: 12,
+            currentIndex: 1,
+            handleClick: jest.fn()
+        };
+    })
+    it('renders correctly', () => {
+        let tree = renderer.create(
+            <Answer {...answerProps} />
+        );
+        expect(tree.toJSON()).toMatchSnapshot();
+    });
+
+    it('click should call passedin click handler', () => {
+        let mockClickHandler = jest.fn();
+        answerProps.handleClick = mockClickHandler;
+        let wrapper = shallow(<Answer {...answerProps} />);
+        let elm = wrapper.find('.Answer--Btn')
+        elm.simulate("click");
+        expect(mockClickHandler).toHaveBeenCalled()
+    })
+});
--- src/Components/Answer/index.js
+++ src/Components/Answer/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import './Answer.css';
 
 class Answer extends Component {
@@ -22,5 +23,11 @@ class Answer extends Component {
     )
   }
 }
+
+Answer.propTypes = {
+  value: PropTypes.number.isRequired,
+  currentIndex: PropTypes.number.isRequired,
+  handleClick: PropTypes.func.isRequired
+};
 
 export default Answer;

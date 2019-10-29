992a6bc9132258c62836ec39a8bd1b967baf5720
pankajladhar <ladharpankaj@gmail.com>
2019-10-29 09:57:49 +0530

    Add offline banner

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -1,29 +1,58 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from "react";
 import { HashRouter as Router, Route, Link, Switch } from "react-router-dom";
-import Quiz from './../Quiz'
-import Categories from './../Categories';
-import './App.css';
+import Quiz from "./../Quiz";
+import Categories from "./../Categories";
+import "./App.css";
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOnline: true
+    };
+  }
+  componentDidMount() {
+    this.setState({
+      isOnline: navigator.onLine
+    });
+  }
   render() {
     return (
-      <div className="App">
-        <Router>     
+      <Fragment>
+        {!this.state.isOnline && (
+          <div className="Offline-Banner">
+            You are offline. You might see different experience :) !!!
+          </div>
+        )}
+        <div className="App">
+          <Router>
             <Switch>
               <Route path={"/"} exact component={Categories} />
-              <Route path={"/add"} render={(props) => <Quiz {...props} operator="+" />} />
-              <Route path={"/sub"} render={(props) => <Quiz {...props} operator="-" />} />
-              <Route path={"/multiply"} render={(props) => <Quiz {...props} operator="x" />} />
-              <Route path={"/comparison"} render={(props) => <Quiz {...props} operator="___" />} />
+              <Route
+                path={"/add"}
+                render={props => <Quiz {...props} operator="+" />}
+              />
+              <Route
+                path={"/sub"}
+                render={props => <Quiz {...props} operator="-" />}
+              />
+              <Route
+                path={"/multiply"}
+                render={props => <Quiz {...props} operator="x" />}
+              />
+              <Route
+                path={"/comparison"}
+                render={props => <Quiz {...props} operator="___" />}
+              />
               {/* <Route path="/division" render={(props) => <Quiz {...props} operand="/" />}/> */}
               <Route component={Categories} />
             </Switch>
-        </Router>
-      </div>
+          </Router>
+        </div>
+      </Fragment>
     );
   }
 }
 
 export default App;
 
-
- /* */
+/* */
